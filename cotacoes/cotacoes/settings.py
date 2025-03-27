e5de932d0261609de6b1d363af95746c469f4354
Luisbuscher <luisbuscher@gmail.com>
2025-03-27 18:57:45 -0300

    feat: alteracao de core para permitir acesso do localhost port 3000

--- cotacoes/cotacoes/settings.py
+++ cotacoes/cotacoes/settings.py
@@ -38,11 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'api',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +55,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # URL do seu frontend React
 ]
 
 ROOT_URLCONF = 'cotacoes.urls'
